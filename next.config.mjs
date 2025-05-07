41bac72f608df203fc3371d003014c38e9c59d95
84DuLu <95832761+84DuLu@users.noreply.github.com>
2025-05-07 18:04:38 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
   },
   // 如果你的网站不是部署在域名根目录，而是在子目录（如 username.github.io/repo-name），
   // 则需要添加 basePath 配置
-  // basePath: '/your-repo-name',
+  basePath: '/fifa_card2',
 }
 
 export default nextConfig
